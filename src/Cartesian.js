9643f786796beed7478c28f31141549752070509
Jorge Yau <codenameyau@gmail.com>
2020-02-17 12:27:25 -0500

    Run prettier

--- src/Cartesian.js
+++ src/Cartesian.js
@@ -1,29 +1,29 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import styled, { css } from 'styled-components';
+import React from "react";
+import PropTypes from "prop-types";
+import styled, { css } from "styled-components";
 
 export const CartesianGrid = styled.div`
-	margin: auto;
-	display: grid;
-	grid-template-columns: ${({ cols = 4 }) => `repeat(${cols}, 1fr)`};
-	grid-auto-rows: minmax(50px, auto);
-	justify-items: stretch;
-	align-items: stretch;
-	background: ${({ background }) => background};
-	gap: 0.1em;
+  margin: auto;
+  display: grid;
+  grid-template-columns: ${({ cols = 4 }) => `repeat(${cols}, 1fr)`};
+  grid-auto-rows: minmax(50px, auto);
+  justify-items: stretch;
+  align-items: stretch;
+  background: ${({ background }) => background};
+  gap: 0.1em;
 `;
 
 export const CartesianGridItem = styled.div`
-	border: 1px solid #eee;
-	padding: 1em;
-	transition: background 0.3s;
-	position: relative;
-	cursor: pointer;
-	&:hover {
-		background: #eee;
-		${({ showProps }) =>
-			showProps &&
-			css`
+  border: 1px solid #eee;
+  padding: 1em;
+  transition: background 0.3s;
+  position: relative;
+  cursor: pointer;
+  &:hover {
+    background: #eee;
+    ${({ showProps }) =>
+      showProps &&
+      css`
 			&::before {
 				content: '${({ content }) => `hello\n${content}`}';
 				white-space: pre;
@@ -43,114 +43,114 @@ export const CartesianGridItem = styled.div`
 				z-index: 1;
 			}
 		`}
-		&::after {
-			content: 'Click to copy';
-			font-size: 12px;
-			color: #aaa;
-			position: absolute;
-			top: 2px;
-			right: 2px;
-		}
-	}
-	&:active {
-		background: #bbb;
-	}
+    &::after {
+      content: "Click to copy";
+      font-size: 12px;
+      color: #aaa;
+      position: absolute;
+      top: 2px;
+      right: 2px;
+    }
+  }
+  &:active {
+    background: #bbb;
+  }
 `;
 
 export const getCartesianProduct = arrays => {
-	if (!arrays || !arrays.length) {
-		return [];
-	}
+  if (!arrays || !arrays.length) {
+    return [];
+  }
 
-	return arrays.reduce(
-		(acc, value) =>
-			acc
-				.map(x => {
-					return value.map(y => {
-						return Array.isArray(y) ? x.concat([y]) : x.concat(y);
-					});
-				})
-				.reduce((acc, val) => acc.concat(val), []),
-		[[]]
-	);
+  return arrays.reduce(
+    (acc, value) =>
+      acc
+        .map(x => {
+          return value.map(y => {
+            return Array.isArray(y) ? x.concat([y]) : x.concat(y);
+          });
+        })
+        .reduce((acc, val) => acc.concat(val), []),
+    [[]]
+  );
 };
 
 export const getCartesianProps = (props = {}) => {
-	const keys = Object.keys(props);
-	const cartesianProduct = getCartesianProduct(Object.values(props));
+  const keys = Object.keys(props);
+  const cartesianProduct = getCartesianProduct(Object.values(props));
 
-	return cartesianProduct.map(result => {
-		return result.reduce((acc, value, idx) => {
-			return {
-				...acc,
-				[keys[idx]]: value,
-			};
-		}, {});
-	});
+  return cartesianProduct.map(result => {
+    return result.reduce((acc, value, idx) => {
+      return {
+        ...acc,
+        [keys[idx]]: value
+      };
+    }, {});
+  });
 };
 
 export const getJSX = (Component, { children, ...props }) => {
-	const name = Component.type.displayName || Component.type.name;
+  const name = Component.type.displayName || Component.type.name;
 
-	const propsAttrs = Object.keys(props).reduce((acc, prop) => {
-		const propValue =
-			typeof props[prop] === 'object'
-				? JSON.stringify(props[prop])
-				: props[prop];
+  const propsAttrs = Object.keys(props).reduce((acc, prop) => {
+    const propValue =
+      typeof props[prop] === "object"
+        ? JSON.stringify(props[prop])
+        : props[prop];
 
-		const propAttr =
-			typeof props[prop] === 'string' ? `"${propValue}"` : `{${propValue}}`;
+    const propAttr =
+      typeof props[prop] === "string" ? `"${propValue}"` : `{${propValue}}`;
 
-		return [...acc, `${prop}=` + propAttr];
-	}, []);
+    return [...acc, `${prop}=` + propAttr];
+  }, []);
 
-	const propsString = propsAttrs.join(' ');
+  const propsString = propsAttrs.join(" ");
 
-	return children
-		? `<${name} ${propsString}>${children}</${name}>`
-		: `<${name} ${propsString} />`;
+  return children
+    ? `<${name} ${propsString}>${children}</${name}>`
+    : `<${name} ${propsString} />`;
 };
 
 export const Cartesian = ({ component, props, ...restProps }) => {
-	const Component = component;
-	const cartesianProps = getCartesianProps(props);
-	const { cols, showProps, background } = restProps;
+  const Component = component;
+  const cartesianProps = getCartesianProps(props);
+  const { cols, showProps, background } = restProps;
 
-	const copyComponent = (e, idx) => {
-		const jsx = getJSX(<Component />, cartesianProps[idx]);
-		navigator.clipboard.writeText(jsx);
-	};
+  const copyComponent = (e, idx) => {
+    const jsx = getJSX(<Component />, cartesianProps[idx]);
+    navigator.clipboard.writeText(jsx);
+  };
 
-	return (
-		<CartesianGrid cols={cols} background={background}>
-			{cartesianProps.map(({ children, ...restCartesianProps }, idx) => (
-				<CartesianGridItem
-					key={idx}
-					showProps={showProps}
-					content={JSON.stringify(restCartesianProps, null, 2)}
-					onClick={e => {
-						copyComponent(e, idx);
-					}}
-				>
-					<Component {...restCartesianProps}>{children}</Component>
-				</CartesianGridItem>
-			))}
-		</CartesianGrid>
-	);
+  return (
+    <CartesianGrid cols={cols} background={background}>
+      {cartesianProps.map(({ children, ...restCartesianProps }, idx) => (
+        <CartesianGridItem
+          key={idx}
+          showProps={showProps}
+          content={JSON.stringify(restCartesianProps, null, 2)}
+          onClick={e => {
+            copyComponent(e, idx);
+          }}
+        >
+          <Component {...restCartesianProps}>{children}</Component>
+        </CartesianGridItem>
+      ))}
+    </CartesianGrid>
+  );
 };
 
 Cartesian.propTypes = {
-	component: PropTypes.elementType,
-	props: PropTypes.object,
-	cols: PropTypes.number,
-	showProps: PropTypes.bool,
-	background: PropTypes.string,
+  component: PropTypes.elementType,
+  props: PropTypes.object,
+  cols: PropTypes.number,
+  showProps: PropTypes.bool,
+  background: PropTypes.string
 };
 
 Cartesian.defaultProps = {
-	cols: 4,
-	showProps: false,
-	background: 'none',
+  cols: 4,
+  showProps: false,
+  background: "none"
 };
 
 export default Cartesian;
